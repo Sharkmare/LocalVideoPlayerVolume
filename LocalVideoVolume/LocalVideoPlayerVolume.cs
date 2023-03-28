2d86d2670e457a65bc78fc194de428a62c6bb5fc
darbdarb <bradcooper99@gmail.com>
2023-03-29 09:21:22 +1000

    Config Settings for LocalVolumeSlider

--- LocalVideoVolume/LocalVideoPlayerVolume.cs
+++ LocalVideoVolume/LocalVideoPlayerVolume.cs
@@ -6,12 +6,19 @@ namespace LocalVideoPlayerVolume
 {
     public class LocalVideoPlayerVolume : NeosMod
     {
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<float> Volume = new ModConfigurationKey<float>("Local Default Volume", "Sets the Local Volume for everyone", () => 0f);
+
         public override string Name => "LocalVideoPlayerVolume";
-        public override string Author => "Sox";
-        public override string Version => "1.0.0";
+        public override string Author => "Sox & darbdarb";
+        public override string Version => "1.0.1";
         public override string Link => "";
         public override void OnEngineInit()
         {
+            Config = GetConfiguration();
+            Config.Save(true);
             Harmony harmony = new Harmony("net.Sox.LocalVideoPlayerVolume");
             harmony.PatchAll();
         }
@@ -24,7 +31,8 @@ namespace LocalVideoPlayerVolume
              ValueUserOverride<float> volumeoverride =__instance.Slot.AttachComponent<ValueUserOverride<float>>();
                 volumeoverride.CreateOverrideOnWrite.Value = true;
                 volumeoverride.Target.Target = ____mainAudioOutput.Target.Volume;
-                volumeoverride.Default.Value = 1f;
+                //volumeoverride.Default.Value = 1f;
+                volumeoverride.Default.Value = Config.GetValue(Volume);
             }
         }
     }
